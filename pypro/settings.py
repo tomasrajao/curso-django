ff34fe98890dec94ae2f08ab8fd114829635a1bf
tomasrajao <tomasrajao@gmail.com>
2021-06-24 17:58:56 -0300

    Add (gitignore): diretório .idea/
    Resolvido flake8

--- pypro/settings.py
+++ pypro/settings.py
@@ -18,7 +18,6 @@ from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())  # 'https://tomasdjango2.herokuapp.com'
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -84,7 +81,6 @@ parse_database = partial(dj_database_url.parse, conn_max_age=600)
 DATABASES = {
     'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -127,8 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = f'{BASE_DIR}/staticfiles'
 
-MEDIA_URL= '/media/'
-MEDIA_ROOT= f'{BASE_DIR}/mediafiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = f'{BASE_DIR}/mediafiles'
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
